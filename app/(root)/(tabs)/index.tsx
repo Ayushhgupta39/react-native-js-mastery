a19db57902ec948ec0a39ba59118d31861978524
Ayush Gupta <ayushgupta3902@gmail.com>
2024-12-30 11:07:17 +0530

    feat: complete profile screen and auth flow

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
@@ -10,6 +11,26 @@ export default function Index() {
       }}
     >
       <Text className="text-3xl font-rubik-light p-4">Welcome to Waterbnb</Text>
+      {/* <Link
+        className="p-3 m-2 border-[.5px] w-1/2 text-center"
+        href={"/sign-in"}
+      >
+        Sign In Page
+      </Link>
+      <Link
+        className="p-3 m-2 border-[.5px] w-1/2 text-center"
+        href={"/explore"}
+      >
+        {" "}
+        Explore Page
+      </Link>
+      <Link href={"/profile"}>Profile page</Link>
+      <Link
+        className="p-3 m-2 border-[.5px] w-1/2 text-center"
+        href={"/properties/1"}
+      >
+        Propert page
+      </Link> */}
     </View>
   );
 }
