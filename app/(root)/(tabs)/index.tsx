e32ce167da68f71b92ab6683b43279b44f35c23b
Ayush Gupta <ayushgupta3902@gmail.com>
2024-12-31 00:37:50 +0530

    feat: add cards, searchbar and filter to homepage

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,36 +1,78 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Card, FeaturedCard } from "@/components/cards";
+import Filters from "@/components/filters";
+import SearchBar from "@/components/SearchBar";
+import icons from "@/constants/icons";
+import images from "@/constants/images";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text className="text-3xl font-rubik-light p-4">Welcome to Waterbnb</Text>
-      {/* <Link
-        className="p-3 m-2 border-[.5px] w-1/2 text-center"
-        href={"/sign-in"}
-      >
-        Sign In Page
-      </Link>
-      <Link
-        className="p-3 m-2 border-[.5px] w-1/2 text-center"
-        href={"/explore"}
-      >
-        {" "}
-        Explore Page
-      </Link>
-      <Link href={"/profile"}>Profile page</Link>
-      <Link
-        className="p-3 m-2 border-[.5px] w-1/2 text-center"
-        href={"/properties/1"}
-      >
-        Propert page
-      </Link> */}
-    </View>
+    <SafeAreaView className="bg-white h-full">
+      <View className="pl-5 pr-5">
+        {/* Header */}
+        <View className="flex flex-row items-center justify-between mt-5">
+          <View className="flex flex-row">
+            <Image source={images.avatar} className="size-12 rounded-full" />
+            <View className="flex flex-row justify-between items-center mx-4">
+              <View className="flex flex-col">
+                <Text className="font-rubik-light">Good Morning</Text>
+                <Text className="font-rubik-medium text-xl">Ayush Gupta</Text>
+              </View>
+            </View>
+          </View>
+          <Image source={icons.bell} className="size-7" />
+        </View>
+
+        {/* Searchbar */}
+        <SearchBar />
+
+        {/* Homepage featured section */}
+        <ScrollView className="h-full">
+          <View className="my-5">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Featured
+              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+
+            <ScrollView
+              horizontal={true}
+              showsHorizontalScrollIndicator={false}
+            >
+              <View className="flex flex-row gap-5 mt-5">
+                <FeaturedCard />
+                <FeaturedCard />
+                <FeaturedCard />
+              </View>
+            </ScrollView>
+          </View>
+
+          <View className="my-5">
+            <View className="flex flex-row items-center justify-between">
+              <Text className="text-xl font-rubik-bold text-black-300">
+                Our Recommendations
+              </Text>
+              <TouchableOpacity>
+                <Text className="text-base font-rubik-bold text-primary-300">
+                  See All
+                </Text>
+              </TouchableOpacity>
+            </View>
+              <Filters />
+
+            <View className="flex flex-row gap-5 mt-5">
+              <Card />
+              <Card />
+            </View>
+          </View>
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
